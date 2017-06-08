cdb7317acfc7820a950dba7b578fce5ee2a61a87
Zayne Berndt <berndt.zayne@gmail.com>
2017-06-08 14:23:28 +0800

    authentication bug fix

--- app/scripts/controllers/UserCtrl.js
+++ app/scripts/controllers/UserCtrl.js
@@ -1,25 +1,10 @@
 (function() {
-      function UserCtrl($scope) {
+      function UserCtrl() {
+         
           
-          $scope.user = function();
-          $scope.firstName = "John";
-          $scope.secondName = "Doe";
-         $scope.email = "JohnDoe@gmail.com";
-          
-      $scope.user = function() {
-        ($scope.firstName && $scope.secondName && $scope.email)
-         }
-      
-     $scope.userId = 0;
-     var userId = $scope.userId;
-     if (user == 0){
-     userId = (userId + 1);
-     console.log(userId);
-     };
-          
-      
+      }
   
       angular
-          .module('Found')
+          .module('partybeats')
           .controller('UserCtrl', UserCtrl);
   })(); 
